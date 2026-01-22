c8de5c916c58bf74212ce39a7886a6b8c52633a6
Vladimir Kolesnikov <9858713@gmail.com>
2026-01-22 15:35:22 +0200

    -auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UserModule } from './user/user.module';
 import { MediaModule } from './media/media.module';
 import { TypeormConfig } from './typeorm.config';
 import { StorageModule } from './storage/storage.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeormConfig, UserModule, MediaModule, StorageModule],
+  imports: [TypeormConfig, UserModule, MediaModule, StorageModule, AuthModule],
 })
 export class AppModule {}
