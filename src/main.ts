c06ed0d361d65085dc787f2750e4fb8247b65d62
Vladimir Kolesnikov <9858713@gmail.com>
2026-01-27 15:47:17 +0200

    ---add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AuthModule } from './auth/auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,18 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('media-app API')
+    .setContact('CapyBit Ltd', 'https://github.com/VladimirKolesnikov/media-app-b', 'e@mail.com')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [AuthModule]
+  });
+  SwaggerModule.setup('/documentation', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
